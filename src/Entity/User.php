59f4d2cb460ad3a546b27ac2eaf620eaf05a5781
Mansour Yade <yadeserignemansour@gmail.com>
2020-01-08 03:38:59 +0000

    mise a jour de la table user avec l'ajout des fonctionnalités de création des utlisateurs et des traitements associés (US2 et US3)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,16 +3,54 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity(fields={"username"}, message="Cet utilisateur existe déja")
+ * @ApiResource(
+ *      collectionOperations = {
+ *      "get"= {"security"= "is_granted(['ROLE_SUP_ADMIN'])"},
+ * 
+ *      "CreatedAdmin"={"method"= "POST", 
+ *      "path"="/users/admin/new",
+ *      "security"= "is_granted(['ROLE_SUP_ADMIN'])" },
+ * 
+ *      "CreatedCaissier"={"method"= "POST", 
+ *      "path"="/users/caissier/new", 
+ *      "security"="is_granted(['ROLE_SUP_ADMIN','ROLE_ADMIN'])"}
+ *     },
+ * 
+ *       itemOperations = {
+ *         "get"={
+ *            "security"= "is_granted(['ROLE_SUP_ADMIN','Role_ADMIN'])",
+ *             "security_message"="Vous n'etes pas autorister à lister des utilisateurs, droit reservé au SUP_ADMIN et l'ADMIN" 
+ *            },
+ * 
+ *          "blockedAdmin"={
+ *             "method"="PUT",
+ *              "path"="/users/admin/{id}",
+ *              "security_message"="accés refusé, seule le SUP_ADMIN peut bloquer un adminitrateur"
+ *           },
+ *           
+ *          "blockedCaissier"={
+ *             "method"="PUT",
+ *              "path"="/users/caissier/{id}",
+ *              "security_message"="seule le SUP_ADMIN et l'Admin peuvent bloquer un adminitrateur"
+ *           },
+ *          
+ *          "delete"={"security"= "is_granted(['ROLE_SUP_ADMIN','Role_ADMIN'])"}
+ *              
+ *      } 
+ *  )
  */
-class User implements UserInterface
+class User implements AdvancedUserInterface
 {
     /**
+     * 
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
@@ -34,6 +72,17 @@ class User implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Role", inversedBy="users")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $role;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $is_active;
 
     public function getId(): ?int
     {
@@ -62,11 +111,11 @@ class User implements UserInterface
      */
     public function getRoles(): array
     {
-        $roles = $this->roles;
+        $roles = [$this->role->getLibelle()];
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
+        return $roles;
     }
 
     public function setRoles(array $roles): self
@@ -107,4 +156,47 @@ class User implements UserInterface
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
     }
+
+    public function getRole(): ?Role
+    {
+        return $this->role;
+    }
+
+    public function setRole(?Role $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->is_active;
+    }
+
+    public function setIsActive(bool $is_active): self
+    {
+        $this->is_active = $is_active;
+
+        return $this;
+    }
+
+
+    public function isAccountNonExpired(){
+        return true;
+    }
+    public function isAccountNonLocked(){
+        return true;
+    }
+    public function isCredentialsNonExpired(){
+
+        return true;
+    }
+    
+
+    public function isEnabled(){
+    
+        return  $this->is_active;
+    }
+
 }
