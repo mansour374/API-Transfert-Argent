b76ad89b9796085a96dbc9e12800ad810cb6b45b
Mansour Yade <yadeserignemansour@gmail.com>
2020-02-08 16:28:54 +0000

    mise a jour entite role

--- src/Entity/Role.php
+++ src/Entity/Role.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ApiResource()
@@ -27,6 +28,7 @@ class Role
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\User", mappedBy="role")
+     * 
      */
     private $users;
 
